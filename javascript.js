4eaa0ff5c40ce779d2d2bb29b82fe39e8664e33d
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-01-21 11:31:19 +1300

    Added buttons

--- javascript.js
+++ javascript.js
@@ -1,18 +1,29 @@
 $(document).ready(function (){
 	newGrid(20);
 
-	$(".cell").mouseenter(function(){
-		$(this).removeClass("blank");
-		$(this).addClass("selected");
+	$("#newBtn").on("click", function(){
+		var size=prompt("Please select grid size","20");
+		newGrid(size);
+
+	   $('a.toggler').click(function(){
+        $(this).toggleClass('a.toggler.off');
+    });
+
 	});
+	
 });
 
 function newGrid(size){
-for (var j = 1; j<= size; j++){
+	$("#main").empty();
+	for (var j = 1; j<= size; j++){
 		for (var i = 1; i <= size; i++) {
 			var id = "'"+i+"'";
 			$("#main").append("<div id="+id+" class='blank cell'></div>");
 		};
 	};
+	$(".cell").mouseenter(function(){
+		$(this).removeClass("blank");
+		$(this).addClass("selected");
+	});
 
 }
