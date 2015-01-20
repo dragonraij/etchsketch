709c7fa6321ed5cdbbe5b59948c80c5cc0127641
Raj Prasad <rajeshwarrprasad@yahoo.com>
2015-01-20 14:23:50 +1300

    hover effect

--- javascript.js
+++ javascript.js
@@ -1,8 +1,13 @@
 $(document).ready(function (){
-for (var j = 1; j<= 20; j++){
-for (var i = 1; i <= 20; i++) {
-	var id = "'"+i+"'";
-	$("#main").append("<div id="+id+" class='blank'></div>");
-};
-};
+	for (var j = 1; j<= 20; j++){
+		for (var i = 1; i <= 20; i++) {
+			var id = "'"+i+"'";
+			$("#main").append("<div id="+id+" class='blank'></div>");
+		};
+	};
+
+	$("#blank").mouseenter(function(){
+		$(this).removeClass("blank");
+		$(this).addClass("selected");
+	});
 });
